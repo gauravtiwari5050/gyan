8b9a22f65ebbd018c5e8b5a1ad052436285db28b
gauravt <gaurav2gaurav@gmail.com>
2012-02-09 22:05:07 +0530

    adding signup.css to precompile array

--- config/application.rb
+++ config/application.rb
@@ -49,5 +49,6 @@ module GyanV1
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.precompile += ['signup.css']
   end
 end
