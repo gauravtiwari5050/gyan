4764a4956c8451b029a3a33ce5d44234b5149230
gauravt <gaurav2gaurav@gmail.com>
2012-02-11 23:55:24 +0530

    report auto resize

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -7,11 +7,6 @@ $(document).ready(function() {
 	
 	$('#calendar').fullCalendar({
 		editable: true,        
-		header: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'month,agendaWeek,agendaDay'
-        },
         defaultView: 'month',
         height: 500,
         slotMinutes: 15,
