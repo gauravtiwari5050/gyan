2ea24cea6cdda2964bc59d274fd9b3219b8b953a
gauravt <gaurav2gaurav@gmail.com>
2012-02-29 20:17:36 +0530

    fix for sessions 1

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-GyanV1::Application.config.session_store :cookie_store, :key => '_gyan_v1_session'
+GyanV1::Application.config.session_store :cookie_store, :key => '_gyan_v1_session',:domain => :all
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
